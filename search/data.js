e6158a17f2f9b48e0201dd6b064cb9f28ad82d7f
kamekititamiko <lonleystars903309@gmail.com>
2022-01-30 18:35:22 +0900

    Netfly

--- search/data.js
+++ search/data.js
@@ -2,151 +2,115 @@
 
 var data = [
   {
-    url: "https://kamekititamiko.github.io/kamenora/categories/",
+    url: "https://objective-bardeen-e63d98.netlify.app/about/",
+    title: "About",
+    date: "2021-01-30T00:00:00Z",
+    body: "About ▽Info 個人の趣味を発信しているサイトです。 現在のところ以下のようなカテゴリーでサイトを運営しています。 カテゴリーは随時追加していく予定です。 ▼Auther"
+  },
+  {
+    url: "https://objective-bardeen-e63d98.netlify.app/",
+    title: "The Note of KamekitiTamiko",
+    date: "2021-01-30T00:00:00Z",
+    body: "The Note of KamekitiTamiko"
+  },
+  {
+    url: "https://objective-bardeen-e63d98.netlify.app/categories/",
     title: "Categories",
     date: "2019-03-11T00:00:00Z",
     body: "Categories"
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/travel/markdown-syntax/",
+    url: "https://objective-bardeen-e63d98.netlify.app/travel/markdown-syntax/",
     title: "Markdown Syntax Guide",
     date: "2019-03-11T00:00:00Z",
     body: "Markdown Syntax Guide This article offers a sample of basic Markdown syntax that can be used in Hugo content files, also it shows whether basic HTML elements are decorated with CSS in a Hugo theme. Headings The following HTML \u0026lt;h1\u0026gt;—\u0026lt;h6\u0026gt; elements represent six levels of section headings. \u0026lt;h1\u0026gt; is the highest section level while \u0026lt;h6\u0026gt; is the lowest. H1 H2 H3 H4 H5 H6 Paragraph Xerum, quo qui aut unt expliquam qui dolut labo. Aque venitatiusda cum, voluptionse latur sitiae dolessi aut parist aut dollo enim qui voluptate ma dolestendit peritin re plis aut quas inctum laceat est volestemque commosa as cus endigna tectur, offic to cor sequas etum rerum idem sintibus eiur? Quianimin porecus evelectur, cum que nis nust voloribus ratem aut omnimi, sitatur? Quiatem. Nam, omnis sum am facea corem alique molestrunt et eos evelece arcillit ut aut eos eos nus, sin conecerem erum fuga. Ri oditatquam, ad quibus unda veliamenimin cusam et facea ipsamus es exerum sitate dolores editium rerore eost, temped molorro ratiae volorro te reribus dolorer sperchicium faceata tiustia prat. Itatur? Quiatae cullecum rem ent aut odis in re eossequodi nonsequ idebis ne sapicia is sinveli squiatum, core et que aut hariosam ex eat. Blockquotes The blockquote element represents content that is quoted from another source, optionally with a citation which must be within a footer or cite element, and optionally with in-line changes such as annotations and abbreviations. Blockquote without attribution Tiam, ad mint andaepu dandae nostion secatur sequo quae. Note that you can use Markdown syntax within a blockquote. Blockquote with attribution Don\u0026rsquo;t communicate by sharing memory, share memory by communicating. — Rob Pike1 Tables Tables aren\u0026rsquo;t part of the core Markdown spec, but Hugo supports supports them out-of-the-box. Name Age Bob 27 Alice 23 Inline Markdown within tables Inline  Markdown  In  Table italics bold strikethrough  code Code Blocks Code block with backticks \u0026lt;!DOCTYPE html\u0026gt;\r\u0026lt;html lang=\u0026quot;en\u0026quot;\u0026gt;\r\u0026lt;head\u0026gt;\r\u0026lt;meta charset=\u0026quot;UTF-8\u0026quot;\u0026gt;\r\u0026lt;title\u0026gt;Example HTML5 Document\u0026lt;/title\u0026gt;\r\u0026lt;/head\u0026gt;\r\u0026lt;body\u0026gt;\r\u0026lt;p\u0026gt;Test\u0026lt;/p\u0026gt;\r\u0026lt;/body\u0026gt;\r\u0026lt;/html\u0026gt;\rCode block indented with four spaces \u0026lt;!DOCTYPE html\u0026gt;\r\u0026lt;html lang=\u0026quot;en\u0026quot;\u0026gt;\r\u0026lt;head\u0026gt;\r\u0026lt;meta charset=\u0026quot;UTF-8\u0026quot;\u0026gt;\r\u0026lt;title\u0026gt;Example HTML5 Document\u0026lt;/title\u0026gt;\r\u0026lt;/head\u0026gt;\r\u0026lt;body\u0026gt;\r\u0026lt;p\u0026gt;Test\u0026lt;/p\u0026gt;\r\u0026lt;/body\u0026gt;\r\u0026lt;/html\u0026gt;\r Code block with Hugo\u0026rsquo;s internal highlight shortcode \u0026lt;!DOCTYPE html\u0026gt; \u0026lt;html lang=\u0026#34;en\u0026#34;\u0026gt; \u0026lt;head\u0026gt; \u0026lt;meta charset=\u0026#34;UTF-8\u0026#34;\u0026gt; \u0026lt;title\u0026gt;Example HTML5 Document\u0026lt;/title\u0026gt; \u0026lt;/head\u0026gt; \u0026lt;body\u0026gt; \u0026lt;p\u0026gt;Test\u0026lt;/p\u0026gt; \u0026lt;/body\u0026gt; \u0026lt;/html\u0026gt; List Types Ordered List First item Second item Third item Unordered List List item Another item And another item Nested list Item First Sub-item Second Sub-item Other Elements — abbr, sub, sup, kbd, mark GIF is a bitmap image format. H2O Xn + Yn = Zn Press CTRL+ALT+Delete to end the session. Most salamanders are nocturnal, and hunt for insects, worms, and other small creatures. The above quote is excerpted from Rob Pike\u0026rsquo;s talk during Gopherfest, November 18, 2015. \u0026#x21a9;\u0026#xfe0e;"
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/categories/syntax/",
+    url: "https://objective-bardeen-e63d98.netlify.app/categories/syntax/",
     title: "syntax",
     date: "2019-03-11T00:00:00Z",
     body: "syntax"
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/categories/themes/",
+    url: "https://objective-bardeen-e63d98.netlify.app/categories/themes/",
     title: "themes",
     date: "2019-03-11T00:00:00Z",
     body: "themes"
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/travel/",
+    url: "https://objective-bardeen-e63d98.netlify.app/travel/",
     title: "Travel",
     date: "2019-03-11T00:00:00Z",
     body: "Travel"
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/tags/privacy/",
+    url: "https://objective-bardeen-e63d98.netlify.app/tags/privacy/",
     title: "privacy",
     date: "2019-03-10T00:00:00Z",
     body: "privacy"
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/travel/rich-content/",
+    url: "https://objective-bardeen-e63d98.netlify.app/travel/rich-content/",
     title: "Rich Content",
     date: "2019-03-10T00:00:00Z",
     body: "Rich Content 下は\u0026quot;google-maps-location\u0026quot;の結果。施設名で表示させたい場所が表示させられる場合はlocationに施設名を記入。 そうでなければ緯度経度を記入する。 \r\r \r\r \r\r 下は\u0026quot;google-maps-search\u0026quot;の結果 \r \r \r 下は\u0026quot;google-maps-pb\u0026quot;の結果 allowfullscreen=\"\"\rloading=\"lazy\"\r\r 下は\u0026quot;google-maps-direction\u0026quot;の結果 \r Hugo ships with several Built-in Shortcodes for rich content, along with a Privacy Config and a set of Simple Shortcodes that enable static and no-JS versions of various social media embeds. */}}\r--\r YouTube Privacy Enhanced Shortcode Twitter Simple Shortcode .twitter-tweet { font: 14px/1.45 -apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,Oxygen-Sans,Ubuntu,Cantarell,\"Helvetica Neue\",sans-serif; border-left: 4px solid #2b7bb9; padding-left: 1.5em; color: #555; } .twitter-tweet a { color: #2b7bb9; text-decoration: none; } blockquote.twitter-tweet a:hover, blockquote.twitter-tweet a:focus { text-decoration: underline; } “In addition to being more logical, asymmetry has the advantage that its complete appearance is far more optically effective than symmetry.” — Jan Tschichold pic.twitter.com/gcv7SrhvJb \u0026mdash; Graphic Design History (@DesignReviewed) January 17, 2019 Vimeo Simple Shortcode .__h_video { position: relative; padding-bottom: 56.23%; height: 0; overflow: hidden; width: 100%; background: #000; } .__h_video img { width: 100%; height: auto; color: #000; } .__h_video .play { height: 72px; width: 72px; left: 50%; top: 50%; margin-left: -36px; margin-top: -36px; position: absolute; cursor: pointer; }"
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/tags/shortcodes/",
+    url: "https://objective-bardeen-e63d98.netlify.app/tags/shortcodes/",
     title: "shortcodes",
     date: "2019-03-10T00:00:00Z",
     body: "shortcodes"
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/tags/",
+    url: "https://objective-bardeen-e63d98.netlify.app/tags/",
     title: "Tags",
     date: "2019-03-10T00:00:00Z",
     body: "Tags"
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/tags/markdown/",
+    url: "https://objective-bardeen-e63d98.netlify.app/tags/markdown/",
     title: "markdown",
     date: "2019-03-09T00:00:00Z",
     body: "markdown"
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/travel/placeholder-text/",
+    url: "https://objective-bardeen-e63d98.netlify.app/travel/placeholder-text/",
     title: "Placeholder Text",
     date: "2019-03-09T00:00:00Z",
     body: "Placeholder Text Lorem est tota propiore conpellat pectoribus de pectora summo. Redit teque digerit hominumque toris verebor lumina non cervice subde tollit usus habet Arctonque, furores quas nec ferunt. Quoque montibus nunc caluere tempus inhospita parcite confusaque translucet patri vestro qui optatis lumine cognoscere flos nubis! Fronde ipsamque patulos Dryopen deorum. Exierant elisi ambit vivere dedere Duce pollice Eris modo Spargitque ferrea quos palude Rursus nulli murmur; hastile inridet ut ab gravi sententia! Nomine potitus silentia flumen, sustinet placuit petis in dilapsa erat sunt. Atria tractus malis. Comas hunc haec pietate fetum procerum dixit Post torum vates letum Tiresia Flumen querellas Arcanaque montibus omnes Quidem et Vagus elidunt The Van de Graaf Canon Mane refeci capiebant unda mulcebat Victa caducifer, malo vulnere contra dicere aurato, ludit regale, voca! Retorsit colit est profanae esse virescere furit nec; iaculi matertera et visa est, viribus. Divesque creatis, tecta novat collumque vulnus est, parvas. Faces illo pepulere tempus adest. Tendit flamma, ab opes virum sustinet, sidus sequendo urbis. Iubar proles corpore raptos vero auctor imperium; sed et huic: manus caeli Lelegas tu lux. Verbis obstitit intus oblectamina fixis linguisque ausus sperare Echionides cornuaque tenent clausit possit. Omnia putatur. Praeteritae refert ausus; ferebant e primus lora nutat, vici quae mea ipse. Et iter nil spectatae vulnus haerentia iuste et exercebat, sui et. Eurytus Hector, materna ipsumque ut Politen, nec, nate, ignari, vernum cohaesit sequitur. Vel mitis temploque vocatus, inque alis, oculos nomen non silvis corpore coniunx ne displicet illa. Crescunt non unus, vidit visa quantum inmiti flumina mortis facto sic: undique a alios vincula sunt iactata abdita! Suspenderat ego fuit tendit: luna, ante urbem Propoetides parte."
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/tags/text/",
+    url: "https://objective-bardeen-e63d98.netlify.app/tags/text/",
     title: "text",
     date: "2019-03-09T00:00:00Z",
     body: "text"
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/camera/",
-    title: "Camera",
-    date: "2019-03-05T00:00:00Z",
-    body: "Camera"
-  },
-  {
-    url: "https://kamekititamiko.github.io/kamenora/cycling/",
-    title: "Cycling",
-    date: "2019-03-05T00:00:00Z",
-    body: "Cycling"
-  },
-  {
-    url: "https://kamekititamiko.github.io/kamenora/tags/emoji/",
+    url: "https://objective-bardeen-e63d98.netlify.app/tags/emoji/",
     title: "emoji",
     date: "2019-03-05T00:00:00Z",
     body: "emoji"
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/camera/emoji-support/",
+    url: "https://objective-bardeen-e63d98.netlify.app/travel/emoji-support/",
     title: "Emoji Support",
     date: "2019-03-05T00:00:00Z",
     body: "Emoji Support Emoji can be enabled in a Hugo project in a number of ways. The emojify function can be called directly in templates or Inline Shortcodes. To enable emoji globally, set enableEmoji to true in your site’s configuration and then you can type emoji shorthand codes directly in content files; e.g. 🙈 🙈 🙉 🙉 🙊 🙊 The Emoji cheat sheet is a useful reference for emoji shorthand codes. N.B. The above steps enable Unicode Standard emoji characters and sequences in Hugo, however the rendering of these glyphs depends on the browser and the platform. To style the emoji you can either use a third party emoji font or a font stack; e.g. .emoji { font-family: Apple Color Emoji,Segoe UI Emoji,NotoColorEmoji,Segoe UI Symbol,Android Emoji,EmojiSymbols; }"
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/cycling/emoji-support/",
-    title: "Emoji Support",
-    date: "2019-03-05T00:00:00Z",
-    body: "Emoji Support Emoji can be enabled in a Hugo project in a number of ways. The emojify function can be called directly in templates or Inline Shortcodes. To enable emoji globally, set enableEmoji to true in your site’s configuration and then you can type emoji shorthand codes directly in content files; e.g. 🙈 🙈 🙉 🙉 🙊 🙊 The Emoji cheat sheet is a useful reference for emoji shorthand codes. N.B. The above steps enable Unicode Standard emoji characters and sequences in Hugo, however the rendering of these glyphs depends on the browser and the platform. To style the emoji you can either use a third party emoji font or a font stack; e.g. .emoji { font-family: Apple Color Emoji,Segoe UI Emoji,NotoColorEmoji,Segoe UI Symbol,Android Emoji,EmojiSymbols; }"
-  },
-  {
-    url: "https://kamekititamiko.github.io/kamenora/nature/emoji-support/",
-    title: "Emoji Support",
-    date: "2019-03-05T00:00:00Z",
-    body: "Emoji Support Emoji can be enabled in a Hugo project in a number of ways. The emojify function can be called directly in templates or Inline Shortcodes. To enable emoji globally, set enableEmoji to true in your site’s configuration and then you can type emoji shorthand codes directly in content files; e.g. 🙈 🙈 🙉 🙉 🙊 🙊 The Emoji cheat sheet is a useful reference for emoji shorthand codes. N.B. The above steps enable Unicode Standard emoji characters and sequences in Hugo, however the rendering of these glyphs depends on the browser and the platform. To style the emoji you can either use a third party emoji font or a font stack; e.g. .emoji { font-family: Apple Color Emoji,Segoe UI Emoji,NotoColorEmoji,Segoe UI Symbol,Android Emoji,EmojiSymbols; }"
-  },
-  {
-    url: "https://kamekititamiko.github.io/kamenora/travel/emoji-support/",
-    title: "Emoji Support",
-    date: "2019-03-05T00:00:00Z",
-    body: "Emoji Support Emoji can be enabled in a Hugo project in a number of ways. The emojify function can be called directly in templates or Inline Shortcodes. To enable emoji globally, set enableEmoji to true in your site’s configuration and then you can type emoji shorthand codes directly in content files; e.g. 🙈 🙈 🙉 🙉 🙊 🙊 The Emoji cheat sheet is a useful reference for emoji shorthand codes. N.B. The above steps enable Unicode Standard emoji characters and sequences in Hugo, however the rendering of these glyphs depends on the browser and the platform. To style the emoji you can either use a third party emoji font or a font stack; e.g. .emoji { font-family: Apple Color Emoji,Segoe UI Emoji,NotoColorEmoji,Segoe UI Symbol,Android Emoji,EmojiSymbols; }"
-  },
-  {
-    url: "https://kamekititamiko.github.io/kamenora/nature/",
-    title: "Nature",
-    date: "2019-03-05T00:00:00Z",
-    body: "Nature"
-  },
-  {
-    url: "https://kamekititamiko.github.io/kamenora/about/",
-    title: "About",
-    date: "2019-02-28T00:00:00Z",
-    body: "About このページはまだ作成途中です。しばらくおまちください! Written in Go, Hugo is an open source static site generator available under the Apache Licence 2.0. Hugo supports TOML, YAML and JSON data file types, Markdown and HTML content files and uses shortcodes to add rich content. Other notable features are taxonomies, multilingual mode, image processing, custom output formats, HTML/CSS/JS minification and support for Sass SCSS workflows. Hugo makes use of a variety of open source projects including: https://github.com/yuin/goldmark https://github.com/alecthomas/chroma https://github.com/muesli/smartcrop https://github.com/spf13/cobra https://github.com/spf13/viper Hugo is ideal for blogs, corporate websites, creative portfolios, online magazines, single page applications or even a website with thousands of pages. Hugo is for people who want to hand code their own website without worrying about setting up complicated runtimes, dependencies and databases. Websites built with Hugo are extremelly fast, secure and can be deployed anywhere including, AWS, GitHub Pages, Heroku, Netlify and any other hosting provider. Learn more and contribute on GitHub."
-  },
-  {
-    url: "https://kamekititamiko.github.io/kamenora/",
-    title: "The Note of KamekitiTamiko",
-    date: "2019-02-28T00:00:00Z",
-    body: "The Note of KamekitiTamiko"
-  },
-  {
-    url: "https://kamekititamiko.github.io/kamenora/search/data.js",
+    url: "https://objective-bardeen-e63d98.netlify.app/search/data.js",
     title: "",
     date: "0001-01-01T00:00:00Z",
     body: ""
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/contact/",
+    url: "https://objective-bardeen-e63d98.netlify.app/contact/",
     title: "Contact",
     date: "0001-01-01T00:00:00Z",
     body: "Contact"
   },
   {
-    url: "https://kamekititamiko.github.io/kamenora/search/",
+    url: "https://objective-bardeen-e63d98.netlify.app/search/",
     title: "Search",
     date: "0001-01-01T00:00:00Z",
     body: "Search"
