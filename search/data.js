e451c07e8ee84cbc5a897d0b3fe9f69a8b240649
kamekititamiko <lonleystars903309@gmail.com>
2022-02-05 13:36:28 +0900

    shirahama commit

--- search/data.js
+++ search/data.js
@@ -14,7 +14,7 @@ var data = [
     body: "Travel"
   },
   {
-    url: "https://objective-bardeen-e63d98.netlify.app/travel/shirahama/",
+    url: "https://objective-bardeen-e63d98.netlify.app/travel/shirahama/shirahama/",
     title: "お気に入りの場所・白浜について",
     date: "2021-02-05T00:00:00Z",
     body: "お気に入りの場所・白浜について 白浜（和歌山県田辺市白浜町）が好きで年1回くらいの頻度で訪れているのですが、ここでは白浜お気に入りスポットを紹介したいと思います。 cafeFAB ビートルズ好きのマスターが運営されているカフェです。 いつもバラエティー豊かなケーキが準備されています。クリームチーズのケーキがおいしかったです。 View this post on Instagram A post shared by cafeFAB (@cafefab1998) \r\r 円月島 円月島は波に浸食されて真ん中に穴があいた不思議な形の島です。夏の夕方に夕日がちょうどこの穴から顔を出したときの景色が余りにも有名ですね。 下の写真は戦前に撮られたのか？と思うほどガサガサしたモノクロ写真ですが、2021年の冬に撮影したものです。かなりクセのある特徴のフィルムで撮ったんです。 \r\r 臨海浦海水浴場 臨海浦海水浴場は、白浜の中心街から円月島を超え、さらに進んだところにあります。 静かな砂浜で、プライベートビーチのようです。 近くには下の写真のような奇岩があります。まわりは磯になっていて、ウニやカニがたくさんいます。また江戸時代に武器庫として使用された？といわれる長～い洞窟も付近にひっそりとあるので、探検気分を味わうこともできます。 \r\r 他にもおすすめの場所はありますが、今回は初めての投稿なので、この程度の紹介にしておいて、また別の機会に紹介したいと思います。"
@@ -24,12 +24,6 @@ var data = [
     title: "白浜",
     date: "2021-02-05T00:00:00Z",
     body: "白浜"
-  },
-  {
-    url: "https://objective-bardeen-e63d98.netlify.app/travel/shirahama/shirahama/",
-    title: "私のお気に入りの場所・白浜について",
-    date: "2021-02-05T00:00:00Z",
-    body: "私のお気に入りの場所・白浜について Emoji can be enabled in a Hugo project in a number of ways. The emojify function can be called directly in templates or Inline Shortcodes. To enable emoji globally, set enableEmoji to true in your site’s configuration and then you can type emoji shorthand codes directly in content files; e.g. 🙈 🙈 🙉 🙉 🙊 🙊 The Emoji cheat sheet is a useful reference for emoji shorthand codes. N.B. The above steps enable Unicode Standard emoji characters and sequences in Hugo, however the rendering of these glyphs depends on the browser and the platform. To style the emoji you can either use a third party emoji font or a font stack; e.g. .emoji { font-family: Apple Color Emoji,Segoe UI Emoji,NotoColorEmoji,Segoe UI Symbol,Android Emoji,EmojiSymbols; }"
   },
   {
     url: "https://objective-bardeen-e63d98.netlify.app/about/",
@@ -44,52 +38,16 @@ var data = [
     body: "The Note of KamekitiTamiko"
   },
   {
-    url: "https://objective-bardeen-e63d98.netlify.app/categories/",
-    title: "Categories",
-    date: "2019-03-11T00:00:00Z",
-    body: "Categories"
-  },
-  {
-    url: "https://objective-bardeen-e63d98.netlify.app/travel/markdown-syntax/",
-    title: "Markdown Syntax Guide",
-    date: "2019-03-11T00:00:00Z",
-    body: "Markdown Syntax Guide This article offers a sample of basic Markdown syntax that can be used in Hugo content files, also it shows whether basic HTML elements are decorated with CSS in a Hugo theme. Headings The following HTML \u0026lt;h1\u0026gt;—\u0026lt;h6\u0026gt; elements represent six levels of section headings. \u0026lt;h1\u0026gt; is the highest section level while \u0026lt;h6\u0026gt; is the lowest. H1 H2 H3 H4 H5 H6 Paragraph Xerum, quo qui aut unt expliquam qui dolut labo. Aque venitatiusda cum, voluptionse latur sitiae dolessi aut parist aut dollo enim qui voluptate ma dolestendit peritin re plis aut quas inctum laceat est volestemque commosa as cus endigna tectur, offic to cor sequas etum rerum idem sintibus eiur? Quianimin porecus evelectur, cum que nis nust voloribus ratem aut omnimi, sitatur? Quiatem. Nam, omnis sum am facea corem alique molestrunt et eos evelece arcillit ut aut eos eos nus, sin conecerem erum fuga. Ri oditatquam, ad quibus unda veliamenimin cusam et facea ipsamus es exerum sitate dolores editium rerore eost, temped molorro ratiae volorro te reribus dolorer sperchicium faceata tiustia prat. Itatur? Quiatae cullecum rem ent aut odis in re eossequodi nonsequ idebis ne sapicia is sinveli squiatum, core et que aut hariosam ex eat. Blockquotes The blockquote element represents content that is quoted from another source, optionally with a citation which must be within a footer or cite element, and optionally with in-line changes such as annotations and abbreviations. Blockquote without attribution Tiam, ad mint andaepu dandae nostion secatur sequo quae. Note that you can use Markdown syntax within a blockquote. Blockquote with attribution Don\u0026rsquo;t communicate by sharing memory, share memory by communicating. — Rob Pike1 Tables Tables aren\u0026rsquo;t part of the core Markdown spec, but Hugo supports supports them out-of-the-box. Name Age Bob 27 Alice 23 Inline Markdown within tables Inline  Markdown  In  Table italics bold strikethrough  code Code Blocks Code block with backticks \u0026lt;!DOCTYPE html\u0026gt;\r\u0026lt;html lang=\u0026quot;en\u0026quot;\u0026gt;\r\u0026lt;head\u0026gt;\r\u0026lt;meta charset=\u0026quot;UTF-8\u0026quot;\u0026gt;\r\u0026lt;title\u0026gt;Example HTML5 Document\u0026lt;/title\u0026gt;\r\u0026lt;/head\u0026gt;\r\u0026lt;body\u0026gt;\r\u0026lt;p\u0026gt;Test\u0026lt;/p\u0026gt;\r\u0026lt;/body\u0026gt;\r\u0026lt;/html\u0026gt;\rCode block indented with four spaces \u0026lt;!DOCTYPE html\u0026gt;\r\u0026lt;html lang=\u0026quot;en\u0026quot;\u0026gt;\r\u0026lt;head\u0026gt;\r\u0026lt;meta charset=\u0026quot;UTF-8\u0026quot;\u0026gt;\r\u0026lt;title\u0026gt;Example HTML5 Document\u0026lt;/title\u0026gt;\r\u0026lt;/head\u0026gt;\r\u0026lt;body\u0026gt;\r\u0026lt;p\u0026gt;Test\u0026lt;/p\u0026gt;\r\u0026lt;/body\u0026gt;\r\u0026lt;/html\u0026gt;\r Code block with Hugo\u0026rsquo;s internal highlight shortcode \u0026lt;!DOCTYPE html\u0026gt; \u0026lt;html lang=\u0026#34;en\u0026#34;\u0026gt; \u0026lt;head\u0026gt; \u0026lt;meta charset=\u0026#34;UTF-8\u0026#34;\u0026gt; \u0026lt;title\u0026gt;Example HTML5 Document\u0026lt;/title\u0026gt; \u0026lt;/head\u0026gt; \u0026lt;body\u0026gt; \u0026lt;p\u0026gt;Test\u0026lt;/p\u0026gt; \u0026lt;/body\u0026gt; \u0026lt;/html\u0026gt; List Types Ordered List First item Second item Third item Unordered List List item Another item And another item Nested list Item First Sub-item Second Sub-item Other Elements — abbr, sub, sup, kbd, mark GIF is a bitmap image format. H2O Xn + Yn = Zn Press CTRL+ALT+Delete to end the session. Most salamanders are nocturnal, and hunt for insects, worms, and other small creatures. The above quote is excerpted from Rob Pike\u0026rsquo;s talk during Gopherfest, November 18, 2015. \u0026#x21a9;\u0026#xfe0e;"
-  },
-  {
-    url: "https://objective-bardeen-e63d98.netlify.app/categories/syntax/",
-    title: "syntax",
-    date: "2019-03-11T00:00:00Z",
-    body: "syntax"
-  },
-  {
-    url: "https://objective-bardeen-e63d98.netlify.app/categories/themes/",
-    title: "themes",
-    date: "2019-03-11T00:00:00Z",
-    body: "themes"
-  },
-  {
-    url: "https://objective-bardeen-e63d98.netlify.app/tags/privacy/",
-    title: "privacy",
-    date: "2019-03-10T00:00:00Z",
-    body: "privacy"
-  },
-  {
-    url: "https://objective-bardeen-e63d98.netlify.app/travel/rich-content/",
-    title: "Rich Content",
-    date: "2019-03-10T00:00:00Z",
-    body: "Rich Content 下は\u0026quot;google-maps-location\u0026quot;の結果。施設名で表示させたい場所が表示させられる場合はlocationに施設名を記入。 そうでなければ緯度経度を記入する。 \r\r \r\r \r\r 下は\u0026quot;google-maps-search\u0026quot;の結果 \r\r \r\r \r\r 下は\u0026quot;google-maps-pb\u0026quot;の結果 allowfullscreen=\"\"\rloading=\"lazy\"\r\r\r 下は\u0026quot;google-maps-direction\u0026quot;の結果 \r\r Hugo ships with several Built-in Shortcodes for rich content, along with a Privacy Config and a set of Simple Shortcodes that enable static and no-JS versions of various social media embeds. */}}\r--\r YouTube Privacy Enhanced Shortcode Twitter Simple Shortcode .twitter-tweet { font: 14px/1.45 -apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,Oxygen-Sans,Ubuntu,Cantarell,\"Helvetica Neue\",sans-serif; border-left: 4px solid #2b7bb9; padding-left: 1.5em; color: #555; } .twitter-tweet a { color: #2b7bb9; text-decoration: none; } blockquote.twitter-tweet a:hover, blockquote.twitter-tweet a:focus { text-decoration: underline; } “In addition to being more logical, asymmetry has the advantage that its complete appearance is far more optically effective than symmetry.” — Jan Tschichold pic.twitter.com/gcv7SrhvJb \u0026mdash; Graphic Design History (@DesignReviewed) January 17, 2019 Vimeo Simple Shortcode .__h_video { position: relative; padding-bottom: 56.23%; height: 0; overflow: hidden; width: 100%; background: #000; } .__h_video img { width: 100%; height: auto; color: #000; } .__h_video .play { height: 72px; width: 72px; left: 50%; top: 50%; margin-left: -36px; margin-top: -36px; position: absolute; cursor: pointer; }"
-  },
-  {
-    url: "https://objective-bardeen-e63d98.netlify.app/tags/shortcodes/",
-    title: "shortcodes",
-    date: "2019-03-10T00:00:00Z",
-    body: "shortcodes"
-  },
-  {
     url: "https://objective-bardeen-e63d98.netlify.app/search/data.js",
     title: "",
     date: "0001-01-01T00:00:00Z",
     body: ""
+  },
+  {
+    url: "https://objective-bardeen-e63d98.netlify.app/categories/",
+    title: "Categories",
+    date: "0001-01-01T00:00:00Z",
+    body: "Categories"
   },
   {
     url: "https://objective-bardeen-e63d98.netlify.app/contact/",
